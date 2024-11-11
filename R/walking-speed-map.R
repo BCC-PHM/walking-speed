2980ade07d606dd2aef2857825139e71923e52ef
David.Ellis <david.ellis@birmingham.gov.uk>
2024-11-11 16:33:23 +0000

    Updated walking speed estimate and added 65+ adult distribution

--- R/walking-speed-map.R
+++ R/walking-speed-map.R
@@ -9,22 +9,25 @@ credits <- paste("Contains OS data \u00A9 Crown copyright and database right\n",
                  " Walking speed estimtates based on Asher et. al (2012)."
 )
 
-census <- read_excel("../data/BSol-census-age-lsoa.xlsx") %>%
+census <- read_excel("../data/BSol-census-age-sex-lsoa.xlsx") %>%
   mutate(
     Age = `Age (B) (7 categories)`,
-    LSOA21 = `Lower layer Super Output Areas Code`
+    LSOA21 = `Lower layer Super Output Areas Code`,
+    Sex = `Sex (2 categories)`
   ) %>%
-  select(LSOA21, Age, Observation)
+  select(LSOA21, Age, Sex, Observation)
 
 too_slow <- data.frame(
-  Age = unique(census$Age),
-  fraction = c(0.0, 0.77, 0.82, 0.87, 0.91, 0.98, 0.98)
+  Age = c(unique(census$Age), unique(census$Age)),
+  Sex = c(rep("Male", 7), rep("Female", 7)),
+  fraction = c(0.0, 0.77, 0.82, 0.87, 0.91, 0.98, 0.98,
+               0.0, 0.87, 0.89, 0.96, 0.98, 1.00, 1.00)
 )
 
 too_slow_perc <- census %>%
   left_join(
     too_slow,
-    by = join_by(Age)
+    by = join_by(Age, Sex)
   ) %>%
   mutate(
     too_slow = Observation * fraction
@@ -40,9 +43,40 @@ map <- plot_map(
   too_slow_perc,
   "Estimated % Walking less than 1.2m/s",
   "LSOA21",
-  map_title = "Estimated Percentage of Population Walking less than 1.2m/s",
+  map_title = "Residents with Walking Impairment (%)\n(Unable to walk at 1.2m/s)",
   credits = credits,
   style = "cont"
 )
 map
-save_map(map, "../figures/walking-speed-map.png")
+save_map(map, "../output/walking-speed-map.png")
+
+
+#############################################################
+#           Percentage of residents aged 65+                #
+#############################################################
+
+total <- census %>%
+  group_by(LSOA21) %>%
+  summarize(total = sum(Observation))
+
+older_adults <- census %>%
+  filter(!grepl("under", Age)) %>%
+  group_by(LSOA21) %>%
+  summarize(OA = sum(Observation))
+
+older_adult_perc <- total %>%
+  left_join(older_adults,
+            by = join_by(LSOA21)) %>%
+  mutate(
+    OA_perc = 100 * OA / total
+  )
+
+OA_map <- plot_map(
+  older_adult_perc,
+  "OA_perc",
+  "LSOA21",
+  map_title = "Residents Aged 65+ (%)",
+  style = "cont"
+)
+OA_map
+save_map(OA_map, "../output/older-adults-map.png")
